f3e15546987a9aa4dd349b7f67ccf03dbdfdeab4
sasial-roblox <44125644+sasial-dev@users.noreply.github.com>
2021-03-16 14:33:51 +1000

    Add Bot

--- index.js
+++ index.js
@@ -2,9 +2,39 @@ const express = require('express')
 const app = express()
 const axios = require('axios').default
 const query = require('querystring')
+const discord = require("discord.js")
+const Database = require("@replit/database")
+const db = new Database()
 const clientId = "541415639525621760"
 const clientSecret = process.env.clientSecret
+
 var redirectUrl = "https://github-verification-bot.sasial.repl.co/callback"
+var token = process.env.TOKEN
+
+// Bot
+
+var client = new discord.Client()
+
+client.login(token)
+
+client.on("message", async message => {
+    if (!message.guild) return;
+    if (message.bot) return;
+    if (message.content == "!verify-contributor") {
+        if (!message.member.roles.cache.has("821234698491396108")) {
+            if (await db.get(message.member.id)) {
+                await message.member.roles.add("821234698491396108")
+                message.channel.send("Done!")
+            } else {
+                message.channel.send("Please go to https://github-verification-bot.sasial.repl.co/login, and then re-run the command after you finish the prompts!")
+            }
+        } else {
+            message.channel.send("You already have the role!")
+        }
+    }
+})
+
+// Server
 
 app.get("/login", async (req,res) => {
     res.redirect("https://discord.com/oauth2/authorize?client_id=541415639525621760&redirect_uri=https%3A%2F%2Fgithub-verification-bot.sasial.repl.co%2Fcallback&response_type=code&scope=identify%20connections&prompt=none")
@@ -22,44 +52,57 @@ app.get("/callback", async (req,res) => {
         'Content-Type': 'application/x-www-form-urlencoded'
         }
     }).catch(err => {
-        console.error(err.request.data)
+        console.error(err)
     })
     
     if (data) {
-        var connectionFound = false
-        const connections = await axios.get('https://discord.com/api/v8/users/@me/connections', {
+        const user = await axios.get('https://discord.com/api/v8/users/@me', {
             headers: {
                 Authorization: `Bearer ${data.data.access_token}`
             }
         }).catch(err => {
-          console.error(err.request.data)
+            console.error(err)
         })
-        if (connections) {
-            var i = 0
-            for (i = 0; i < connections.data.length; i++) {
-                if (connections.data[i].type == "github") {
-                    const contributors = await axios.get('https://api.github.com/repos/sasial-dev/Vanilla-Updater/contributors').catch(err => {
-                        console.error(err.request.data)
-                    })
-                    if (contributors) {
-                        var j = 0
-                        var contributorIds = []
-                        for (j = 0; j < contributors.data.length; j++) {
-                            contributorIds.push(contributors.data[j].id)
-                        }
-                        if (contributorIds.includes(Number(connections.data[i].id))) {
-                            connectionFound = true
-                            res.json({
-                                contributor: true
-                            })
+        if (user) {
+            var connectionFound = false
+            const connections = await axios.get('https://discord.com/api/v8/users/@me/connections', {
+                headers: {
+                    Authorization: `Bearer ${data.data.access_token}`
+                }
+            }).catch(err => {
+            console.error(err)
+            })
+            if (connections) {
+                var i = 0
+                for (i = 0; i < connections.data.length; i++) {
+                    if (connections.data[i].type == "github") {
+                        const contributors = await axios.get('https://api.github.com/repos/sasial-dev/Vanilla-Updater/contributors', {
+                            auth: {
+                                username: "205e43a274d60cdbdcde",
+                                password: process.env.GitHubSecret
+                            }
+                        }).catch(err => {
+                            console.error(err)
+                        })
+                        if (contributors) {
+                            var j = 0
+                            var contributorIds = []
+                            for (j = 0; j < contributors.data.length; j++) {
+                                contributorIds.push(contributors.data[j].id)
+                            }
+                            if (contributorIds.includes(Number(connections.data[i].id))) {
+                                connectionFound = true
+                                await db.set(user.data.id, true)
+                                res.send("Sucess!")
+                            }
                         }
                     }
                 }
-            }
-            if (!connectionFound) {
-              res.json({
-                    contributor: false
-              })
+                if (!connectionFound) {
+                    res.json({
+                            contributor: false
+                    })
+                }
             }
         }
     }
